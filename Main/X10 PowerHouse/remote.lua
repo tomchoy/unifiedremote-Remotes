27def6896bbc188eff1615dc31e0f71f357210fc
tchoy <tchoymailme@gmail.com>
2023-12-18 21:09:52 -0800

    Move CM15A Module to 192.168.168.89 (WD-DX4000) and UnifiedRemoteServer to tommy-lggram

--- Main/X10 PowerHouse/remote.lua
+++ Main/X10 PowerHouse/remote.lua
@@ -1,82 +1,82 @@
 
 local script = libs.script;
 
-local netcat_path = "set path=%path%;C:\\Users\\Tommy\\bin\\nc111nt"
+local netcat_path = "set path=%path%;C:\\Users\\tchoy\\bin\\nc111nt"
 
 actions.officeLampOn = function ()
-	script.default(netcat_path, "echo pl c1 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c1 on | nc -w 1 192.168.168.89 1099");
 end
 actions.officeLampOff = function ()
-	script.default(netcat_path, "echo pl c1 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c1 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.landscapeLightingOn = function ()
-	script.default(netcat_path, "echo pl c3 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c3 on | nc -w 1 192.168.168.89 1099");
 end
 actions.landscapeLightingOff = function ()
-	script.default(netcat_path, "echo pl c3 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c3 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.livingTableLampOn = function ()
-	script.default(netcat_path, "echo pl c4 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c4 on | nc -w 1 192.168.168.89 1099");
 end
 actions.livingTableLampOff = function ()
-	script.default(netcat_path, "echo pl c4 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c4 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.livingFloorLampOn = function ()
-	script.default(netcat_path, "echo pl c5 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c5 on | nc -w 1 192.168.168.89 1099");
 end
 actions.livingFloorLampOff = function ()
-	script.default(netcat_path, "echo pl c5 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c5 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.receiverPower = function ()
-	script.default(netcat_path, "echo pl c6 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c6 on | nc -w 1 192.168.168.89 1099");
 end
 
 actions.pumpStart = function ()
-	script.default(netcat_path, "echo pl c7 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c7 on | nc -w 1 192.168.168.89 1099");
 end
 
 actions.denFanOn = function ()
-	script.default(netcat_path, "echo pl c8 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c8 on | nc -w 1 192.168.168.89 1099");
 end
 actions.denFanOff = function ()
-	script.default(netcat_path, "echo pl c8 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c8 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.backyardLightsOn = function ()
-	script.default(netcat_path, "echo pl c9 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c9 on | nc -w 1 192.168.168.89 1099");
 end
 actions.backyardLightsOff = function ()
-	script.default(netcat_path, "echo pl c9 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c9 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.denSpotsOn = function ()
-	script.default(netcat_path, "echo pl c10 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c10 on | nc -w 1 192.168.168.89 1099");
 end
 actions.denSpotsOff = function ()
-	script.default(netcat_path, "echo pl c10 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c10 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.denTableLampOn = function ()
-	script.default(netcat_path, "echo pl c11 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c11 on | nc -w 1 192.168.168.89 1099");
 end
 actions.denTableLampOff = function ()
-	script.default(netcat_path, "echo pl c11 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c11 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.ebikeChargerOn = function ()
-	script.default(netcat_path, "echo pl c12 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c12 on | nc -w 1 192.168.168.89 1099");
 end
 actions.ebikeChargerOff = function ()
-	script.default(netcat_path, "echo pl c12 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c12 off | nc -w 1 192.168.168.89 1099");
 end
 
 actions.guestTableLampOn = function ()
-	script.default(netcat_path, "echo pl c13 on | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c13 on | nc -w 1 192.168.168.89 1099");
 end
 actions.guestTableLampOff = function ()
-	script.default(netcat_path, "echo pl c13 off | nc -w 1 192.168.168.91 1099");
+	script.default(netcat_path, "echo rf c13 off | nc -w 1 192.168.168.89 1099");
 end
