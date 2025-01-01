fdca67df1c56067c2c83646ee0999a42edfdc4f2
tchoy <tchoymailme@gmail.com>
2024-12-31 17:54:28 -0800

    Refactor YouTube remote for Firefox for Windows API

--- Main/YouTube/remote.lua
+++ Main/YouTube/remote.lua
@@ -1,66 +1,80 @@
 local keyboard = libs.keyboard;
+local win = libs.win;
+local device = libs.device;
 
-function toast ()
-	if (settings.toast == "") then
-		libs.server.update({
-			type = "dialog",
-			text = "Volume control requires that the player has focus. Click in the player window using the mouse remote once to focus the player.",
-			title = "Volume",
-			children = { { type = "button", text = "OK" } }
-		});
-		settings.toast = "true";
+events.detect = function ()
+	return
+		libs.fs.exists("C:\\Program Files (x86)\\Mozilla Firefox") or
+		libs.fs.exists("C:\\Program Files\\Mozilla Firefox");
+end
+
+--@help Focus Firefox application
+actions.switch = function()
+	if OS_WINDOWS then
+		local hwnd = win.window("firefox.exe");
+		if (hwnd == 0) then actions.launch(); end
+		win.switchtowait("firefox.exe");
 	end
 end
 
---@help Launch YouTube
+--@help Launch Firefox application
 actions.launch = function()
-	os.open("http://www.youtube.com");
+	if OS_WINDOWS then
+		os.start("firefox.exe");
+	end
 end
 
 --@help Lower volume
 actions.volume_down = function()
-	toast();
+	actions.switch();
 	keyboard.stroke("down");
 end
 
 --@help Raise volume
 actions.volume_up = function()
-	toast();
+	actions.switch();
 	keyboard.stroke("up");
 end
 
 --@help Rewind
 actions.rewind = function()
+	actions.switch();
 	keyboard.stroke("J");
 end
 
 --@help Fast forward
 actions.fast_forward = function()
+	actions.switch();
 	keyboard.stroke("L");
 end
 
 --@help Play previous item
 actions.previous = function()
+	actions.switch();
 	keyboard.stroke("shift", "p");
 end
 
 --@help Play next item
 actions.next = function()
+	actions.switch();
 	keyboard.stroke("shift", "n");
 end
 
 --@help Toggle fullscreen
 actions.fullscreen = function()
+	actions.switch();
 	keyboard.stroke("F");
 end
 
 --@help Exit fullscreen
 actions.exit_fullscreen = function()
+	actions.switch();
 	keyboard.stroke("esc");
 end
 
 --@help Toggle play/pause
 actions.play_pause = function()
+	actions.switch();
 	keyboard.stroke("K");
 end
 
